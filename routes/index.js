a05a589c3b8620e311f011ade52ce123a847401f
vagrant <vagrant@precise32.(none)>
2014-02-06 18:50:57 +0000

    putting it together lab

--- routes/index.js
+++ routes/index.js
@@ -3,5 +3,15 @@ var data = require('../data.json');
 
 exports.view = function(req, res){
 	console.log(data);
-	res.render('index');
+	res.render('index', data);
 };
+
+
+function projectClick(e) {
+	e.preventDefault();
+
+	var text = $(this).text();
+	var newText = anagrammedName(name);
+
+	$(this).text(newText);
+}
